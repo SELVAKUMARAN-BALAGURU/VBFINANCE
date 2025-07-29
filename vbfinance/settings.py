5eaaabb41cc2eadaf92f85d99166b557249caa5b
Selvakumaran B <selvakumaranpkt@gmail.com>
2025-07-29 22:37:43 +0530

    Fix template directory path for Render deployment

--- vbfinance/settings.py
+++ vbfinance/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "vbfinance.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [r'D:\Djangoprojects\vbfinance\templates'],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
